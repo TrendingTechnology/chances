76deb6721e30897fcbc53a0ea20a27744ac15226
Chance Strickland <chance.strickland@gmail.com>
2019-01-23 11:43:52 -0600

    tweak

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -48,7 +48,10 @@ class Footer extends React.Component {
             </ul>
           </nav>
           <span className="Footer__credit">
-            &copy; {new Date().getFullYear()}. Built with React and Gatsby.js. Any visual similarities to other blogging platforms is entirely coincidental.
+            &copy; {new Date().getFullYear()}. Built with React and Gatsby.js.
+            <br />
+            Any visual similarities to other blogging platforms is entirely
+            coincidental.
           </span>
         </div>
       </footer>

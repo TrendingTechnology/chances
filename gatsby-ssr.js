834df62e02bd267f80ea8d6402866a9e9f9a09d4
Chance Strickland <chance.strickland@gmail.com>
2019-01-27 09:15:39 -0600

    add context to SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider, Consumer } from './src/components/Context';
+
 export const onRenderBody = ({ setPostBodyComponents }) => {
   return setPostBodyComponents([
     // TODO: we can add some scripts here but let's be careful
@@ -6,3 +8,15 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
     // In the past there used to be ConvertKit here.
   ]);
 };
+
+export const wrapRootElement = ({ element }) => {
+  return <Provider>{element}</Provider>;
+};
+
+export const wrapPageElement = ({ element, props }) => (
+  <Consumer>
+    {({ darkMode, toggleDarkMode }) =>
+      React.cloneElement(element, { ...props, darkMode, toggleDarkMode })
+    }
+  </Consumer>
+);

bd37c8b496e55cc49a816d67cf937cf6babf258f
Chance Strickland <hi@chancedigital.io>
2019-06-25 09:52:00 -0700

    add entrance transition

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -41,8 +41,7 @@ const Layout = ({
   children,
   handleSearchChange,
 }) => {
-  // eslint-disable-next-line no-unused-vars
-  const [fontsLoaded, fontError, fontState] = usePromise(Fonts(), []);
+  const [fontsLoaded, fontError] = usePromise(Fonts(), []);
   const [menuIsActive, setMenuActiveState] = useState(false);
   const { darkMode } = useContext(ThemeContext);
   const rootPath = `${__PATH_PREFIX__}/`; // eslint-disable-line
@@ -86,54 +85,56 @@ const Layout = ({
         }
       `}
       render={data => (
-        <div className={cx('Layout', className)}>
-          <Helmet
-            bodyAttributes={{
-              class: cx({
-                darkMode,
-                loaded: fontError ? true : Boolean(fontsLoaded),
-              }),
-            }}
-          >
-            {renderFontLinks()}
-          </Helmet>
-          <header
-            className={cx(`Layout__header`, {
-              'Layout__header--wide': location.pathname !== rootPath && image,
-            })}
-          >
-            {header}
-            <hr className="Layout__break" />
-            <div className="Layout__fixed">
-              <GithubButton
-                className="Layout__forkButton"
-                aria-label="Fork on GitHub"
-                label="Fork"
+        <React.Fragment>
+          <aside className="Layout__fixed">
+            <GithubButton
+              className="Layout__forkButton"
+              aria-label="Fork on GitHub"
+              label="Fork"
+            />
+            <Burger
+              className="Layout__burger"
+              onClick={toggleMenu}
+              isActive={menuIsActive}
+              label="Toggle additional controls"
+            />
+            <SidePanel
+              className="Layout__menuWrapper"
+              isActive={menuIsActive}
+              aria-hidden={!menuIsActive}
+            >
+              <DarkModeToggle
+                className="Layout__darkModeToggle"
+                tabIndex={(!menuIsActive && -1).toString()}
               />
-              <Burger
-                className="Layout__burger"
-                onClick={toggleMenu}
-                isActive={menuIsActive}
-                label="Toggle additional controls"
-              />
-              <SidePanel
-                className="Layout__menuWrapper"
-                isActive={menuIsActive}
-                aria-hidden={!menuIsActive}
-              >
-                <DarkModeToggle
-                  className="Layout__darkModeToggle"
-                  tabIndex={(!menuIsActive && -1).toString()}
-                />
-                {/* <nav>
+              {/* <nav>
                   <Menu items={MENU_ITEMS} />
                 </nav> */}
-              </SidePanel>
-            </div>
-          </header>
-          <main className="Layout__main">{children}</main>
-          <Footer className="Layout__footer" />
-        </div>
+            </SidePanel>
+          </aside>
+          <div className={cx('Layout', className)}>
+            <Helmet
+              bodyAttributes={{
+                class: cx({
+                  darkMode,
+                  loaded: fontError ? true : Boolean(fontsLoaded),
+                }),
+              }}
+            >
+              {renderFontLinks()}
+            </Helmet>
+            <header
+              className={cx(`Layout__header`, {
+                'Layout__header--wide': location.pathname !== rootPath && image,
+              })}
+            >
+              {header}
+              <hr className="Layout__break" />
+            </header>
+            <main className="Layout__main">{children}</main>
+            <Footer className="Layout__footer" />
+          </div>
+        </React.Fragment>
       )}
     />
   );
